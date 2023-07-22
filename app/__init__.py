190135d2cf2921e49240462f163c9e84c10003a0
Sam O'Donnell <sam.d.odonnell@gmail.com>
2023-07-21 18:59:43 -0700

    Add CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,28 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 tabula = Flask(__name__)
 tabula.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project.sqlite'
+
+CORS(tabula, origins=['http://localhost:3000'])
+
 db = SQLAlchemy()
 db.init_app(tabula)
 
 with tabula.app_context():
     db.create_all()
 
+
 @tabula.route('/')
 def hello_world():
     return "<p>Hello, world!</p>"
 
+
 @tabula.route('/user/<int:id>')
 def user(id):
     return f"<p>User number {id}</p>"
+
 
 @tabula.route('/dashboard')
 def dashboard():
